266d1a7f66c02dbd4c8b5b101ebe1ab779c5b5c7
DAM105 <DAM105@C0128.iesataulfo.local>
2023-03-20 17:42:10 +0100

    PRIMERA VERSION PROGRAMA

---
+++ Julen_1/src/paquete1/principal1.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class principal1 {
+
+	public static void main(String[] args) {
+		System.out.println("Programa que nos permite hacer un control de versiones con GIT ");
+
+
+		
+	}
+
+}
